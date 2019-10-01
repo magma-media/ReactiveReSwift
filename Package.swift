114dbf0083dca2bfe0e4f048170e8f1272b17f59
Spencer Schoeben <spencer@netspencer.com>
2019-10-01 11:47:21 -0700

    Update Package.swift for use as dependency in iOS proejct

--- Package.swift
+++ Package.swift
@@ -1,10 +1,20 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "ReactiveReSwift",
-    exclude: [
-      "Tests",
-      "Carthage",
-      "Docs"
-    ]
+  name: "ReactiveReSwift",
+  platforms: [
+    .iOS(.v8),
+  ],
+  products: [
+    .library(name: "ReactiveReSwift", targets: ["ReactiveReSwift"]),
+  ],
+  targets: [
+    .target(name: "ReactiveReSwift", path: "Sources"),
+  ]
+  exclude: [
+    "Tests",
+    "Carthage",
+    "Docs",
+  ]
 )
